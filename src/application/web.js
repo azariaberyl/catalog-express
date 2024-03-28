b66733547110bcc2ad963ab3c88c11afd708918d
azariaberyl <beryl.naga@gmail.com>
2024-03-28 11:41:56 +0700

    Add cors origin

--- src/application/web.js
+++ src/application/web.js
@@ -4,8 +4,17 @@ import publicApi from '../route/public-api.js';
 import privateApi from '../route/private-api.js';
 import cors from 'cors';
 
+const allowedOrigin = [
+  'https://catalog-nextjs-kj401zihy-azariaberyls-projects.vercel.app/',
+  'https://catalog-nextjs.vercel.app/',
+];
+
 export const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigin,
+  })
+);
 app.use(express.json());
 app.use(publicApi);
 app.use(privateApi);
