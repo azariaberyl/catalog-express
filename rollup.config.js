dad1c5a35186e9d67511579a8f6ce19cd4639faa
azariaberyl <beryl.naga@gmail.com>
2024-04-15 12:43:56 +0700

    Update: terser dep to newest one

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 // rollup.config.mjs
 export default {
   input: 'src/main.js',
   output: {
     file: 'app.js',
-    format: 'es',
+    format: 'esm',
   },
   plugins: [terser(), nodeResolve(), commonjs(), json()],
 };
